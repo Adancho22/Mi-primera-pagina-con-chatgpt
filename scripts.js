4e40aa92184be022d210660f7c316d1a2ae4b5f3
Adancho22 <adanramirez1211@gmail.com>
2023-11-16 21:02:21 -0500

    primer cambio

---
+++ scripts.js
@@ -0,0 +1,9 @@
+var joinButton = document.getElementById("joinButton");
+
+joinButton.addEventListener("mouseover", function() {
+    this.style.backgroundColor = "#e74c3c";
+});
+
+joinButton.addEventListener("mouseout", function() {
+    this.style.backgroundColor = "#c0392b";
+});
